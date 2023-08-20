kamilburda/gimp-export-layers

Prevent crashes if name preview fails; display warning message instead
The plug-in does not need to crash when an action fails as the action
should not affect the rest of the plug-in functionality. The action can
simply be disabled in this case.

export_layers/gui/preview_name.py
@@ -7,6 +7,7 @@ from future.builtins import *
 
 import collections
 import os
+import traceback
 
 import pygtk
 pygtk.require('2.0')
@@ -17,8 +18,11 @@ from gimp import pdb
 
 from export_layers import pygimplib as pg
 
+from export_layers import exceptions
 from export_layers import utils as utils_
 from export_layers.gui import preview_base as preview_base_
+
+from export_layers.gui import messages as messages_
 
 
 class NamePreview(preview_base_.Preview):
@@ -39,13 +43,18 @@ class NamePreview(preview_base_.Preview):
     by the user or by calling `set_selected_items()`.
   * `'preview-updated'` - The preview was updated by calling `update()`. This
     signal is not emitted if the update is locked.
+    
+    Arguments:
+    
+    * `update_successful` - If `True`, the preview was updated successfully,
+      `False` otherwise (indicating that an error was captured).
   * `'preview-tags-changed'` - An existing tag was added to or removed from an
     item.
   """
   
   __gsignals__ = {
     b'preview-selection-changed': (gobject.SIGNAL_RUN_FIRST, None, ()),
-    b'preview-updated': (gobject.SIGNAL_RUN_FIRST, None, ()),
+    b'preview-updated': (gobject.SIGNAL_RUN_FIRST, None, (gobject.TYPE_BOOLEAN,)),
     b'preview-tags-changed': (gobject.SIGNAL_RUN_FIRST, None, ()),
   }
   
@@ -86,6 +95,7 @@ class NamePreview(preview_base_.Preview):
     self._available_tags_setting = available_tags_setting
     
     self.is_filtering = False
+    self._last_error = None
     
     # key: `Item.raw.ID` or (`Item.raw.ID`, 'folder') instance
     # value: `gtk.TreeIter` instance
@@ -104,10 +114,29 @@ class NamePreview(preview_base_.Preview):
     
     self._init_gui()
   
+  @property
+  def batcher(self):
+    return self._batcher
+  
+  @property
+  def tree_view(self):
+    return self._tree_view
+  
+  @property
+  def collapsed_items(self):
+    return self._collapsed_items
+  
+  @property
+  def selected_items(self):
+    return self._selected_items
+  
+  @property
+  def last_error(self):
+    return self._last_error
+  
   def update(self, reset_items=False, update_existing_contents_only=False):
-    """
-    Update the preview (add/remove item, move item to a different parent item
-    group, etc.).
+    """Updates the preview (add/remove item, move item to a different parent
+    item group, etc.).
     
     If `reset_items` is `True`, perform full update - add new items, remove
     non-existent items, etc. Note that setting this to `True` may introduce a
@@ -117,6 +146,10 @@ class NamePreview(preview_base_.Preview):
     the existing items. Note that the items will not be reparented,
     expanded/collapsed or added/removed even if they need to be. This option is
     useful if you know the item structure will be preserved.
+    
+    If an exception was captured during the update, the method is terminated
+    prematurely. It is the responsibility of the caller to handle the error
+    (e.g. lock or clear the preview).
     """
     update_locked = super().update()
     if update_locked:
@@ -126,6 +159,10 @@ class NamePreview(preview_base_.Preview):
       self.clear()
     
     self._process_items(reset_items=reset_items)
+    
+    if self._last_error:
+      self.emit('preview-updated', False)
+      return
     
     items = self._get_items_to_process()
     
@@ -142,7 +179,7 @@ class NamePreview(preview_base_.Preview):
     
     self._tree_view.columns_autosize()
     
-    self.emit('preview-updated')
+    self.emit('preview-updated', True)
   
   def clear(self):
     """
@@ -179,22 +216,6 @@ class NamePreview(preview_base_.Preview):
       return self._batcher.item_tree[item_key]
     else:
       return None
-  
-  @property
-  def batcher(self):
-    return self._batcher
-  
-  @property
-  def tree_view(self):
-    return self._tree_view
-  
-  @property
-  def collapsed_items(self):
-    return self._collapsed_items
-  
-  @property
-  def selected_items(self):
-    return self._selected_items
   
   def _init_gui(self):
     self._tree_model = gtk.TreeStore(*[column[1] for column in self._COLUMNS])
@@ -537,13 +558,34 @@ class NamePreview(preview_base_.Preview):
       for item in item_tree.iter_all():
         item.reset()
     
-    self._batcher.run(
-      item_tree=item_tree,
-      is_preview=True,
-      process_contents=False,
-      process_names=True,
-      process_export=False,
-      **utils_.get_settings_for_batcher(self._settings['main']))
+    self._last_error = None
+    
+    try:
+      self._batcher.run(
+        item_tree=item_tree,
+        is_preview=True,
+        process_contents=False,
+        process_names=True,
+        process_export=False,
+        **utils_.get_settings_for_batcher(self._settings['main']))
+    except exceptions.BatcherCancelError as e:
+      pass
+    except exceptions.ActionError as e:
+      messages_.display_failure_message(
+        messages_.get_failing_action_message(e),
+        failure_message=str(e),
+        details=traceback.format_exc(),
+        parent=pg.gui.get_toplevel_window(self))
+      
+      self._last_error = e
+    except Exception as e:
+      messages_.display_failure_message(
+        _('There was a problem with updating the name preview:'),
+        failure_message=str(e),
+        details=traceback.format_exc(),
+        parent=pg.gui.get_toplevel_window(self))
+      
+      self._last_error = e
   
   def _update_items(self, items):
     updated_parents = set()

export_layers/gui/previews_controller.py
@@ -9,12 +9,15 @@ from export_layers import pygimplib as pg
 
 from export_layers import builtin_constraints
 from export_layers import builtin_procedures
+from export_layers import exceptions
 
 
 class PreviewsController(object):
   
   _DELAY_PREVIEWS_SETTING_UPDATE_MILLISECONDS = 50
   _DELAY_PREVIEWS_PANE_DRAG_UPDATE_MILLISECONDS = 500
+  
+  _ACTION_ERROR_KEY = 'action_error'
   
   def __init__(self, name_preview, image_preview, settings, image):
     self._name_preview = name_preview
@@ -25,6 +28,8 @@ class PreviewsController(object):
     self._selected_in_preview_constraints = {}
     self._custom_actions = {}
     self._is_initial_selection_set = False
+    
+    self._last_name_preview_update_successful = True
     
     self._paned_outside_previews_previous_position = (
       self._settings['gui/size/paned_outside_previews_position'].value)
@@ -150,8 +155,11 @@ class PreviewsController(object):
     actions_.connect_event('before-remove-action', _on_before_remove_action)
   
   def _update_previews_on_setting_change(self, setting):
+    self._name_preview.lock_update(False, self._ACTION_ERROR_KEY)
     pg.invocation.timeout_add_strict(
       self._DELAY_PREVIEWS_SETTING_UPDATE_MILLISECONDS, self._name_preview.update)
+    
+    self._image_preview.lock_update(False, self._ACTION_ERROR_KEY)
     pg.invocation.timeout_add_strict(
       self._DELAY_PREVIEWS_SETTING_UPDATE_MILLISECONDS, self._image_preview.update)
   
@@ -268,7 +276,11 @@ class PreviewsController(object):
     self._update_selected_items()
     self._update_image_preview()
   
-  def _on_name_preview_updated(self, preview):
+  def _on_name_preview_updated(self, preview, update_successful):
+    if not update_successful and isinstance(self._name_preview.last_error, exceptions.ActionError):
+      self._name_preview.lock_update(True, self._ACTION_ERROR_KEY)
+      self._image_preview.lock_update(True, self._ACTION_ERROR_KEY)
+    
     self._image_preview.update_item()
   
   def _on_name_preview_tags_changed(self, preview):

kamilburda/batcher

batcher/gui/preview_name.py
@@ -2,6 +2,7 @@
 
 import collections
 import os
+import traceback
 
 import pygtk
 pygtk.require('2.0')
@@ -12,7 +13,9 @@ from gimp import pdb
 
 from batcher import pygimplib as pg
 
+from batcher import exceptions
 from batcher import utils as utils_
+from batcher.gui import messages as messages_
 from batcher.gui import preview_base as preview_base_
 
 
@@ -34,13 +37,18 @@ class NamePreview(preview_base_.Preview):
     by the user or by calling `set_selected_items()`.
   * `'preview-updated'` - The preview was updated by calling `update()`. This
     signal is not emitted if the update is locked.
+    
+    Arguments:
+    
+    * `update_successful` - If `True`, the preview was updated successfully,
+      `False` otherwise (indicating that an error was captured).
   * `'preview-tags-changed'` - An existing tag was added to or removed from an
     item.
   """
   
   __gsignals__ = {
     b'preview-selection-changed': (gobject.SIGNAL_RUN_FIRST, None, ()),
-    b'preview-updated': (gobject.SIGNAL_RUN_FIRST, None, ()),
+    b'preview-updated': (gobject.SIGNAL_RUN_FIRST, None, (gobject.TYPE_BOOLEAN,)),
     b'preview-tags-changed': (gobject.SIGNAL_RUN_FIRST, None, ()),
   }
   
@@ -81,6 +89,7 @@ class NamePreview(preview_base_.Preview):
     self._available_tags_setting = available_tags_setting
     
     self.is_filtering = False
+    self._last_error = None
     
     # key: `Item.raw.ID` or (`Item.raw.ID`, 'folder') instance
     # value: `gtk.TreeIter` instance
@@ -97,10 +106,29 @@ class NamePreview(preview_base_.Preview):
     
     self._init_gui()
   
+  @property
+  def batcher(self):
+    return self._batcher
+  
+  @property
+  def tree_view(self):
+    return self._tree_view
+  
+  @property
+  def collapsed_items(self):
+    return self._collapsed_items
+  
+  @property
+  def selected_items(self):
+    return self._selected_items
+  
+  @property
+  def last_error(self):
+    return self._last_error
+  
   def update(self, reset_items=False, update_existing_contents_only=False):
-    """
-    Update the preview (add/remove item, move item to a different parent item
-    group, etc.).
+    """Updates the preview (add/remove item, move item to a different parent
+    item group, etc.).
     
     If `reset_items` is `True`, perform full update - add new items, remove
     non-existent items, etc. Note that setting this to `True` may introduce a
@@ -110,6 +138,10 @@ class NamePreview(preview_base_.Preview):
     the existing items. Note that the items will not be reparented,
     expanded/collapsed or added/removed even if they need to be. This option is
     useful if you know the item structure will be preserved.
+    
+    If an exception was captured during the update, the method is terminated
+    prematurely. It is the responsibility of the caller to handle the error
+    (e.g. lock or clear the preview).
     """
     update_locked = super().update()
     if update_locked:
@@ -119,6 +151,10 @@ class NamePreview(preview_base_.Preview):
       self.clear()
     
     self._process_items(reset_items=reset_items)
+    
+    if self._last_error:
+      self.emit('preview-updated', False)
+      return
     
     items = self._get_items_to_process()
     
@@ -135,7 +171,7 @@ class NamePreview(preview_base_.Preview):
     
     self._tree_view.columns_autosize()
     
-    self.emit('preview-updated')
+    self.emit('preview-updated', True)
   
   def clear(self):
     """
@@ -172,22 +208,6 @@ class NamePreview(preview_base_.Preview):
       return self._batcher.item_tree[item_key]
     else:
       return None
-  
-  @property
-  def batcher(self):
-    return self._batcher
-  
-  @property
-  def tree_view(self):
-    return self._tree_view
-  
-  @property
-  def collapsed_items(self):
-    return self._collapsed_items
-  
-  @property
-  def selected_items(self):
-    return self._selected_items
   
   def _init_gui(self):
     self._tree_model = gtk.TreeStore(*[column[1] for column in self._COLUMNS])
@@ -530,13 +550,34 @@ class NamePreview(preview_base_.Preview):
       for item in item_tree.iter_all():
         item.reset()
     
-    self._batcher.run(
-      item_tree=item_tree,
-      is_preview=True,
-      process_contents=False,
-      process_names=True,
-      process_export=False,
-      **utils_.get_settings_for_batcher(self._settings['main']))
+    self._last_error = None
+    
+    try:
+      self._batcher.run(
+        item_tree=item_tree,
+        is_preview=True,
+        process_contents=False,
+        process_names=True,
+        process_export=False,
+        **utils_.get_settings_for_batcher(self._settings['main']))
+    except exceptions.BatcherCancelError as e:
+      pass
+    except exceptions.ActionError as e:
+      messages_.display_failure_message(
+        messages_.get_failing_action_message(e),
+        failure_message=str(e),
+        details=traceback.format_exc(),
+        parent=pg.gui.get_toplevel_window(self))
+      
+      self._last_error = e
+    except Exception as e:
+      messages_.display_failure_message(
+        _('There was a problem with updating the name preview:'),
+        failure_message=str(e),
+        details=traceback.format_exc(),
+        parent=pg.gui.get_toplevel_window(self))
+      
+      self._last_error = e
   
   def _update_items(self, items):
     updated_parents = set()

batcher/gui/previews_controller.py
@@ -4,12 +4,15 @@ from batcher import pygimplib as pg
 
 from batcher import builtin_constraints
 from batcher import builtin_procedures
+from batcher import exceptions
 
 
 class PreviewsController(object):
   
   _DELAY_PREVIEWS_SETTING_UPDATE_MILLISECONDS = 50
   _DELAY_PREVIEWS_PANE_DRAG_UPDATE_MILLISECONDS = 500
+  
+  _ACTION_ERROR_KEY = 'action_error'
   
   def __init__(self, name_preview, image_preview, settings, image):
     self._name_preview = name_preview
@@ -20,6 +23,8 @@ class PreviewsController(object):
     self._selected_in_preview_constraints = {}
     self._custom_actions = {}
     self._is_initial_selection_set = False
+    
+    self._last_name_preview_update_successful = True
     
     self._paned_outside_previews_previous_position = (
       self._settings['gui/size/paned_outside_previews_position'].value)
@@ -145,8 +150,11 @@ class PreviewsController(object):
     actions_.connect_event('before-remove-action', _on_before_remove_action)
   
   def _update_previews_on_setting_change(self, setting):
+    self._name_preview.lock_update(False, self._ACTION_ERROR_KEY)
     pg.invocation.timeout_add_strict(
       self._DELAY_PREVIEWS_SETTING_UPDATE_MILLISECONDS, self._name_preview.update)
+    
+    self._image_preview.lock_update(False, self._ACTION_ERROR_KEY)
     pg.invocation.timeout_add_strict(
       self._DELAY_PREVIEWS_SETTING_UPDATE_MILLISECONDS, self._image_preview.update)
   
@@ -263,7 +271,11 @@ class PreviewsController(object):
     self._update_selected_items()
     self._update_image_preview()
   
-  def _on_name_preview_updated(self, preview):
+  def _on_name_preview_updated(self, preview, update_successful):
+    if not update_successful and isinstance(self._name_preview.last_error, exceptions.ActionError):
+      self._name_preview.lock_update(True, self._ACTION_ERROR_KEY)
+      self._image_preview.lock_update(True, self._ACTION_ERROR_KEY)
+    
     self._image_preview.update_item()
   
   def _on_name_preview_tags_changed(self, preview):