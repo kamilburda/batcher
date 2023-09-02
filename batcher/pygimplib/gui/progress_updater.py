c148b6dfcf9f0f66d4c741154ed647860722b8ed
kamilburda <8431034+kamilburda@users.noreply.github.com>
2023-09-02 13:07:56 +0200

    Update `pygimplib.gui.progress_updater`

--- batcher/pygimplib/gui/progress_updater.py
+++ batcher/pygimplib/gui/progress_updater.py
@@ -1,5 +1,7 @@
 """GTK progress bar updater."""
 
+from typing import Optional
+
 import gi
 gi.require_version('Gtk', '3.0')
 from gi.repository import Gtk
@@ -17,12 +19,13 @@ class GtkProgressUpdater(pgprogress.ProgressUpdater):
     self.progress_bar.set_fraction(self._num_finished_tasks / self.num_total_tasks)
     self._force_update()
   
-  def _set_text_progress_bar(self, text):
+  def _set_text_progress_bar(self, text: Optional[str]):
+    self.progress_bar.set_show_text(bool(text))
     self.progress_bar.set_text(text)
     self._force_update()
   
-  def _force_update(self):
+  @staticmethod
+  def _force_update():
     # This is necessary for the GTK progress bar to be updated properly.
-    # See http://faq.pygtk.org/index.py?req=show&file=faq23.020.htp
-    while gtk.events_pending():
-      gtk.main_iteration()
+    while Gtk.events_pending():
+      Gtk.main_iteration()
