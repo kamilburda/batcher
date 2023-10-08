a364debccf7afb2cbc8a7b039ec8f16aff05fc06
kamilburda <8431034+kamilburda@users.noreply.github.com>
2023-10-08 12:48:53 +0200

    Update `setting.utils`

--- batcher/pygimplib/setting/utils.py
+++ batcher/pygimplib/setting/utils.py
@@ -2,6 +2,7 @@
 
 import collections
 import itertools
+from typing import Callable, List, Union
 
 __all__ = [
   'SETTING_PATH_SEPARATOR',
@@ -19,15 +20,21 @@ __all__ = [
   'check_setting_name',
 ]
 
-
 SETTING_PATH_SEPARATOR = '/'
+"""Separator for setting paths. See `get_setting_path()` for more information.
+"""
+
 SETTING_ATTRIBUTE_SEPARATOR = '.'
+"""Separator between the name of a `setting.Setting` instance and one of its 
+attributes.
+"""
 
 
 class SettingParentMixin:
-  """
-  This mixin provides `Setting` and `Group` instances with a parent reference,
-  allowing settings and groups to form a tree-like structure.
+  """Mixin providing `setting.Setting` and `setting.Group` instances with a
+  parent reference.
+
+  Parent references allow settings and groups to form a hierarchical structure.
   """
   
   def __init__(self):
@@ -36,13 +43,16 @@ class SettingParentMixin:
     self._parent = None
   
   @property
-  def parent(self):
+  def parent(self) -> 'setting.Group':
+    """The immediate parent (`setting.Group` instance) of the current setting or
+    group.
+    """
     return self._parent
   
   @property
-  def parents(self):
-    """
-    Return a list of parents (setting groups), starting from the topmost parent.
+  def parents(self) -> List['setting.Group']:
+    """Returns a list of parents (`setting.Group` instances), starting from the
+    topmost parent.
     """
     parent = self._parent
     parents = []
@@ -58,9 +68,8 @@ class SettingParentMixin:
 
 
 class SettingEventsMixin:
-  """
-  This mixin provides `Setting` and `Group` instances with the capability of
-  setting up and invoking events.
+  """Mixin for `setting.Setting` and `setting.Group` instances allowing to set
+  up and invoke events.
   """
   
   _event_handler_id_counter = itertools.count(start=1)
@@ -69,54 +78,57 @@ class SettingEventsMixin:
     super().__init__()
     
     # key: event type
-    # value: collections.OrderedDict{
-    #   event handler ID: [event handler, arguments, keyword arguments, is enabled]}
-    self._event_handlers = collections.defaultdict(collections.OrderedDict)
+    # value: {event handler ID: [event handler, arguments, keyword arguments, is enabled]}
+    self._event_handlers = collections.defaultdict(dict)
     
     # This allows faster lookup of events via IDs.
-    # key: event handler ID; value: event type
+    # key: event handler ID
+    # value: event type
     self._event_handler_ids_and_types = {}
   
   def connect_event(
-        self, event_type, event_handler, *event_handler_args, **event_handler_kwargs):
-    """
-    Connect an event handler.
+        self,
+        event_type: str,
+        event_handler: Callable,
+        *event_handler_args,
+        **event_handler_kwargs,
+  ) -> int:
+    """Connects an event handler.
     
-    `event_type` can be an arbitrary string. To invoke an event manually, call
-    `invoke_event`.
+    ``event_type`` can be an arbitrary string. To invoke an event manually, call
+    ``invoke_event``.
     
     Several event types are invoked automatically. For the list of such event
-    types, consult the documentation for `Setting` or `Group` classes.
+    types, consult the documentation for `setting.Setting` or `setting.Group`
+    classes.
     
-    The `event_handler` function must always contain at least one argument -
-    the instance this method is called from (a setting or a setting group).
+    The ``event_handler`` function must always contain at least one argument -
+    the instance this method is called from (a `setting.Setting` or
+    `setting.Group`).
     
     Multiple event handlers can be connected. Each new event handler is invoked
     as the last.
     
-    Parameters:
-    
-    * `event_type` - Event type as a string.
-    
-    * `event_handler` - Function to be called when the event given by
-      `event_type` is invoked.
-    
-    * `*event_handler_args` - Arguments to `event_handler`.
-    
-    * `**event_handler_kwargs` - Keyword arguments to `event_handler`.
+    Args:
+      event_type:
+        Event type as a string.
+      event_handler:
+        Function to be called when the event given by ``event_type`` is invoked.
+      *event_handler_args:
+        Arguments to ``event_handler``.
+      **event_handler_kwargs:
+        Keyword arguments to ``event_handler``.
     
     Returns:
-    
-    * `event_id` - Numeric ID of the event handler (can be used to remove the
-      event via `remove_event`).
+      Numeric ID of the event handler. The ID can be used to remove the event
+      via `remove_event()`.
     
     Raises:
-    
-    * `TypeError` - `event_handler` is not a function or the wrong number of
-      arguments was passed in `event_handler_args`.
+      TypeError: ``event_handler`` is not a function or the wrong number of
+        arguments was passed.
     """
     if not callable(event_handler):
-      raise TypeError('not a function')
+      raise TypeError(f'{event_handler} is not a function')
     
     event_id = next(self._event_handler_id_counter)
     self._event_handlers[event_type][event_id] = [
@@ -125,51 +137,58 @@ class SettingEventsMixin:
     
     return event_id
   
-  def remove_event(self, event_id):
-    """
-    Remove the event handler specified by its ID as returned by
+  def remove_event(self, event_id: int):
+    """Removes the event handler specified by its ID as returned by
     `connect_event()`.
     """
-    if event_id not in self._event_handler_ids_and_types:
-      raise ValueError('event handler with ID {} does not exist'.format(event_id))
+    if not self.has_event(event_id):
+      raise ValueError(f'event handler with ID {event_id} does not exist')
     
     event_type = self._event_handler_ids_and_types[event_id]
     del self._event_handlers[event_type][event_id]
     del self._event_handler_ids_and_types[event_id]
   
-  def set_event_enabled(self, event_id, enabled):
-    """
-    Enable or disable the event handler specified by its ID.
-    
-    If the event ID is already enabled and `enabled` is `True` or is already
-    disabled and `enabled` is `False`, do nothing.
+  def set_event_enabled(self, event_id: int, enabled: bool):
+    """Enables or disables the event handler specified by its ID.
+
+    This method has no effect if the event ID is already enabled and
+    ``enabled`` is ``True`` or is already disabled and ``enabled`` is
+    ``False``.
     
     Raises:
-    
-    * `ValueError` - Event ID is invalid.
+      ValueError:
+        ``event_id`` is not valid.
     """
     if not self.has_event(event_id):
-      raise ValueError('event ID {} is invalid'.format(event_id))
-    
+      raise ValueError(f'event handler with ID {event_id} does not exist')
+
     event_type = self._event_handler_ids_and_types[event_id]
     self._event_handlers[event_type][event_id][3] = enabled
   
-  def has_event(self, event_id):
-    """
-    Return `True` if the event handler specified by its ID is connected to the
-    setting, `False` otherwise.
+  def has_event(self, event_id: int) -> bool:
+    """Returns ``True`` if the event handler specified by its ID exists,
+    ``False`` otherwise.
     """
     return event_id in self._event_handler_ids_and_types
   
-  def invoke_event(self, event_type, *additional_args, **additional_kwargs):
-    """
-    Call all connected event handlers of the specified event type.
-    
-    Additional arguments and keyword arguments are passed via
-    `*additional_args` and `**additional_kwargs`, respectively. These arguments
-    are prepended to the arguments specified in `connect_event` (if any).
-    The same keyword arguments in `connect_event` override keyword arguments in
-    `**additional_kwargs`.
+  def invoke_event(self, event_type: str, *additional_args, **additional_kwargs):
+    """Manually calls all connected event handlers of the specified event type.
+
+    Usually you do not need to call this function unless you implement custom
+    event types (e.g. in a `setting.Setting` subclass) not provided by any
+    existing `setting.Setting` subclasses or the `setting.Group` class.
+
+    Args:
+      event_type:
+        Event type as a string. All event handlers that are enabled and
+        connected with this event type are invoked.
+      *additional_args:
+        Additional positional arguments prepended to the arguments specified in
+        `connect_event()` (if any).
+      **additional_kwargs:
+        Additional keyword arguments prepended to the arguments specified in
+        `connect_event()` (if any). The same keyword arguments in
+        `connect_event()` override keyword arguments in ``**additional_kwargs``.
     """
     for (event_handler,
          args,
@@ -181,53 +200,61 @@ class SettingEventsMixin:
         event_handler(self, *event_handler_args, **event_handler_kwargs)
 
 
-def get_pdb_name(setting_name):
-  """
-  Return name suitable for the description of the setting in the GIMP PDB.
+def get_pdb_name(setting_name: str) -> str:
+  """Return a setting name suitable for the description of the setting in the
+  GIMP Procedural Database (PDB).
   """
   return setting_name.replace('_', '-')
 
 
-def get_setting_name(pdb_name):
-  """
-  Return setting name based on the specified name from GIMP PDB.
+def get_setting_name(pdb_name: str) -> str:
+  """Return a setting name based on the specified name from the GIMP Procedural
+  Database (PDB).
   """
   return pdb_name.replace('-', '_')
 
 
-def value_to_str_prefix(value):
-  """
-  Return stringified setting value useful as a prefix to an error message.
+def value_to_str_prefix(value) -> str:
+  """Returns a string representing setting value, useful as a prefix to an error
+  message.
   
-  If `value` is empty or `None`, return empty string.
+  If ``value`` is empty or ``None``, an empty string is returned.
   """
   if value:
-    return '"{}": '.format(value)
+    return f'"{value}": '
   else:
     return ''
 
 
-def get_processed_display_name(setting_display_name, setting_name):
+def get_processed_display_name(setting_display_name: str, setting_name: str) -> str:
+  """Returns ``setting_display_name`` if not ``None``, otherwise returns a
+  string as returned by `generate_display_name()` using ``setting_name``.
+  """
   if setting_display_name is not None:
     return setting_display_name
   else:
     return generate_display_name(setting_name)
 
 
-def generate_display_name(setting_name):
+def generate_display_name(setting_name: str) -> str:
+  """Returns a string representing the setting name in a format suitable to be
+  displayed to the user (i.e. a display name).
+  """
   return setting_name.replace('_', ' ').capitalize()
 
 
-def get_processed_description(setting_description, setting_display_name):
+def get_processed_description(setting_description: str, setting_display_name: str) -> str:
+  """Returns ``setting_description`` if not ``None``, otherwise returns a
+  string as returned by `generate_description()` using ``setting_display_name``.
+  """
   if setting_description is not None:
     return setting_description
   else:
     return generate_description(setting_display_name)
 
 
-def generate_description(display_name):
-  """
-  Generate setting description from a display name.
+def generate_description(display_name: str) -> str:
+  """Returns setting description from the specified display name.
   
   Underscores in display names used as mnemonics are usually undesired in
   descriptions, hence their removal.
@@ -235,25 +262,34 @@ def generate_description(display_name):
   return display_name.replace('_', '')
 
 
-def get_setting_path(setting, relative_path_group=None):
-  """
-  Get the full setting path consisting of names of parent setting groups and the
-  specified setting. The path components are separated by '/'.
+def get_setting_path(
+      setting: 'setting.Setting',
+      relative_path_group: Union['setting.Group', str, None] = None,
+      separator: str = SETTING_PATH_SEPARATOR,
+) -> str:
+  """Returns the full path of a setting.
+
+  The path consists of names of parent `setting.Group` instances and the
+  ``setting``. The path components are separated by ``separator``.
+
+  For example, if ``setting`` is named 'file_extension' and parent groups
+  (starting from the topmost group) are 'settings' and 'main', then the
+  returned setting path is ``'settings/main/file_extension'`` (if using the
+  default ``separator``).
+
+  If ``relative_path_group`` is a `setting.Group`, it is used to make the
+  setting path relative to this group. If the path of the setting group to
+  the topmost parent does not match, the full path is returned.
   
-  If `relative_path_group` is specified, the setting group is used to
-  relativize the setting path. If the path of the setting group to the topmost
-  parent does not match, return the full path.
-  
-  If `relative_path_group` is equal to `'root'` and the setting has at
-  least one parent, omit the topmost group.
+  If ``relative_path_group`` is ``'root'`` and the setting has at least one
+  parent, the topmost parent is omitted.
   """
   def _get_setting_path(path_components):
-    return SETTING_PATH_SEPARATOR.join([setting.name for setting in path_components])
+    return separator.join([setting_.name for setting_ in path_components])
   
   if relative_path_group == 'root':
     if setting.parents:
-      setting_path_without_root = _get_setting_path(
-        (setting.parents + [setting])[1:])
+      setting_path_without_root = _get_setting_path((setting.parents + [setting])[1:])
       return setting_path_without_root
     else:
       return setting.name
@@ -261,24 +297,23 @@ def get_setting_path(setting, relative_path_group=None):
     setting_path = _get_setting_path(setting.parents + [setting])
     
     if relative_path_group is not None:
-      root_path = _get_setting_path(
-        relative_path_group.parents + [relative_path_group])
+      root_path = _get_setting_path(relative_path_group.parents + [relative_path_group])
       if setting_path.startswith(root_path):
-        return setting_path[len(root_path + SETTING_PATH_SEPARATOR):]
+        return setting_path[len(root_path + separator):]
     
     return setting_path
 
 
-def check_setting_name(setting_name):
-  """
-  Check if the specified setting name is valid. If not, raise `ValueError`.
-  
+def check_setting_name(setting_name: str):
+  """Checks if the specified setting name is valid.
+
   A setting name must not contain `SETTING_PATH_SEPARATOR` or
   `SETTING_ATTRIBUTE_SEPARATOR`.
+
+  If the setting name is not valid, `ValueError` is raised.
   """
   if not isinstance(setting_name, str):
     raise TypeError('setting name must be a string')
   
-  if (SETTING_PATH_SEPARATOR in setting_name
-      or SETTING_ATTRIBUTE_SEPARATOR in setting_name):
-    raise ValueError('setting name "{}" is not valid'.format(setting_name))
+  if SETTING_PATH_SEPARATOR in setting_name or SETTING_ATTRIBUTE_SEPARATOR in setting_name:
+    raise ValueError(f'setting name "{setting_name}" is not valid')
