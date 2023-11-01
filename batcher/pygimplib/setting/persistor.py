92fc5ebf2e32543b426a91fcec4c609138e8323a
kamilburda <8431034+kamilburda@users.noreply.github.com>
2023-11-01 09:36:32 +0100

    Update `setting.persistor`

--- batcher/pygimplib/setting/persistor.py
+++ batcher/pygimplib/setting/persistor.py
@@ -4,8 +4,10 @@ multiple setting sources.
 
 import collections
 from collections.abc import Iterable
+from typing import Dict, List, Union
 
 from . import _sources_errors
+from . import sources as sources_
 
 __all__ = [
   'Persistor',
@@ -15,7 +17,7 @@ __all__ = [
 class Persistor:
   """Loading and saving settings for multiple settings sources at once.
   
-  Settings sources are `setting.sources.Source` instances.
+  Settings sources are `setting.Source` instances.
   
   Apart from merely loading or saving settings, this class also triggers events
   before/after loading/saving for each setting and exits gracefully even if
@@ -24,35 +26,38 @@ class Persistor:
   
   _STATUSES = SUCCESS, PARTIAL_SUCCESS, SOURCE_NOT_FOUND, FAIL, NO_SETTINGS = (0, 1, 2, 3, 4)
   
-  _DEFAULT_SETTING_SOURCES = collections.OrderedDict()
+  _DEFAULT_SETTING_SOURCES = {}
   
   @classmethod
-  def get_default_setting_sources(cls):
-    """Returns a copy of a dictionary containing default setting sources.
-    
+  def get_default_setting_sources(cls) -> Dict[str, Union[sources_.Source, List[sources_.Source]]]:
+    """Returns a dictionary containing default setting sources.
+
+    The returned dictionary is a copy of an internally stored dictionary to
+    prevent it from being modified other than via
+    `set_default_setting_sources()`.
+
     See `set_default_setting_sources()` for more information.
     """
-    return collections.OrderedDict(cls._DEFAULT_SETTING_SOURCES.items())
+    return dict(cls._DEFAULT_SETTING_SOURCES)
   
   @classmethod
-  def set_default_setting_sources(cls, sources):
+  def set_default_setting_sources(cls, sources: Dict[str, sources_.Source, List[sources_.Source]]):
     """Sets the dictionary of setting sources to use in methods of this class if
     no other setting sources in these methods are specified.
     
-    The dictionary must contain pairs of (key, `setting.sources.Source` instance
-    or list of `setting.sources.Source` instances).
+    The dictionary must contain ``(key, setting source(s))`` pairs.
     
     The key is a string that identifies a group of sources. The key can be
-    specified in `setting.settings.Setting` instances within `setting_sources`
+    specified in `setting.Setting` instances within ``setting_sources``
     to indicate which groups of sources the setting can be read from or written
-    to. For example, if the `setting_sources` attribute of a setting contains
-    [`'persistent'`], then only setting sources under the key `'persistent'`
+    to. For example, if the ``setting_sources`` attribute of a setting contains
+    ``['persistent']``, then only setting sources under the ``'persistent'`` key
     will be considered and other sources will be ignored. This is useful if you
     need to e.g. save settings to a different file while still ignoring settings
-    not containing `'persistent'`.
+    not containing ``'persistent'``.
     """
     if sources is None:
-      sources = collections.OrderedDict()
+      sources = {}
     
     if not isinstance(sources, dict):
       raise TypeError('"sources" must be a dictionary')
@@ -60,60 +65,67 @@ class Persistor:
     cls._DEFAULT_SETTING_SOURCES = sources
   
   @classmethod
-  def load(cls, settings_or_groups, setting_sources=None, trigger_events=True):
+  def load(
+        cls,
+        settings_or_groups: Iterable[Union['setting.Setting', 'setting.Group']],
+        setting_sources: Union[
+          Dict[str, Union[sources_.Source, List[sources_.Source]]],
+          List[str],
+          None,
+        ] = None,
+        trigger_events: bool = True,
+  ) -> 'PersistorResult':
     """Loads values from the specified settings or groups, or creates new
     settings within the specified groups if they do not exist.
     
-    The order of setting sources in `settings_or_groups` indicates the
+    The order of setting sources in ``settings_or_groups`` indicates the
     preference of the sources, beginning with the first source. If not all
     settings could be found in the first source, the second source is read to
     assign values to the remaining settings. This continues until all sources
     are read or all settings are found.
     
-    If the setting source(s) contain an invalid value for a setting, the default
-    value for the setting will be assigned.
-    Settings not found in any of the sources will also have their default values
-    assigned.
+    If the setting source(s) contain an invalid value for a setting,
+    the default value for the setting will be assigned. Settings not found in
+    any of the sources will also have their default values assigned.
     
-    The following events are triggered for each `Setting` and `Group` instance
-    within `settings_or_groups`, including child settings and groups:
+    The following events are triggered for each `setting.Setting` and
+    `setting.Group` instance within ``settings_or_groups``, including child
+    settings and groups:
     
-    * `'before-load'` - invoked before loading settings. The event will not be
+    * ``'before-load'``: invoked before loading settings. The event will not be
       triggered for settings present in the source but not in memory as they are
       not loaded yet.
-    
-    * `'after-load'` - invoked after loading settings. The event will also be
+
+    * ``'after-load'`` - invoked after loading settings. The event will also be
       triggered for settings originally not present in memory as they are now
       loaded. This event is triggered even if loading fails for any source.
+
+    * events triggered in `setting.Setting.set_value()` when a setting is being
+      loaded.
+
+    * events triggered in `setting.Setting.reset()` when loading a setting was
+      not successful (occurring when the loaded value was not valid).
     
-    * events triggered in `Setting.set_value()` when a setting is being loaded.
-    
-    * events triggered in `Setting.reset()` when loading a setting was not
-      successful (occurring when the loaded value was not valid).
-    
-    Parameters:
-    
-    * `settings_or_groups` - List of `settings.Setting` or `group.Group`
-      instances whose values are loaded from `setting_sources`. For settings and
-      groups that exist in `settings_or_groups`, only values are loaded.
-      Child settings and groups not present in a group in memory but present in
-      the source(s) are created within the group.
-    
-    * `setting_sources` - Dictionary or list of setting sources or `None`. If a
-      dictionary, it must contain (key, setting source) pairs or
-      (key, list of setting sources) pairs.
-      See `set_default_setting_sources()` for more information on the key.
-      If a list, it must contain keys and all keys must have a
-      mapping to one of the default sources as returned by
-      `get_default_setting_sources()`.
-      If `None`, default setting sources are used as returned by
-      `get_default_setting_sources()`.
-    
-    * `trigger_events` - If `True`, trigger `'before-load'` and `'after-load'`
-      events for each setting. If `False`, these events are not triggered.
-    
+    Args:
+      settings_or_groups:
+        `setting.Setting` and `setting.Group` instances whose values are
+        loaded from ``setting_sources``. For settings and groups that exist
+        in ``settings_or_groups``, only values are loaded. Child settings and
+        groups not present in a group in memory but present in the source(s)
+        are created within the group.
+      setting_sources:
+        Dictionary or list of setting sources or ``None``. If a dictionary,
+        it must contain (key, setting source) pairs or (key, list of setting
+        sources) pairs. See `set_default_setting_sources()` for more
+        information on the key. If a list, it must contain keys and all keys
+        must have a mapping to one of the default sources as returned by
+        `get_default_setting_sources()`. If ``None``, default setting sources
+        are used as returned by `get_default_setting_sources()`.
+      trigger_events:
+        If ``True``, ``'before-load'`` and ``'after-load'`` events are triggered
+        for each setting. If ``False``, these events are not triggered.
+
     Returns:
-    
       A `PersistorResult` instance describing the result, particularly in the
       case of a failure. See `PersistorResult` for more information.
     """
@@ -169,30 +181,39 @@ class Persistor:
     return settings_not_loaded, statuses_per_source, messages_per_source
   
   @classmethod
-  def save(cls, settings_or_groups, setting_sources=None, trigger_events=True):
+  def save(
+        cls,
+        settings_or_groups: Iterable[Union['setting.Setting', 'setting.Group']],
+        setting_sources: Union[
+          Dict[str, Union[sources_.Source, List[sources_.Source]]],
+          List[str],
+          None,
+        ] = None,
+        trigger_events: bool = True,
+  ) -> 'PersistorResult':
     """Saves settings to the specified setting sources.
     
-    The following events for each `Setting` instance (including child settings
-    in `Group` instances) within `settings_or_groups` are triggered:
+    The following events for each `setting.Setting` instance (including child
+    settings in `setting.Group` instances) within ``settings_or_groups`` are
+    triggered:
     
-    * `'before-save'` - invoked before saving settings.
+    * ``'before-save'``: invoked before saving settings.
     
-    * `'after-save'` - invoked after saving settings. This event is triggered
+    * ``'after-save'``: invoked after saving settings. This event is triggered
       even if saving fails for any source.
     
-    Parameters:
-    
-    * `settings_or_groups` - List of `settings.Setting` or `group.Group`
-      instances whose values are saved to `setting_sources`.
-    
-    * `setting_sources` - Dictionary or list of setting sources or `None`. See
-      `load()` for more information.
-    
-    * `trigger_events` - If `True`, trigger `'before-save'` and `'after-save'`
-      events for each setting. If `False`, these events are not triggered.
+    Args:
+      settings_or_groups:
+        List of `settings.Setting` or `group.Group` instances whose values
+        are saved to ``setting_sources``.
+      setting_sources:
+        Dictionary or list of setting sources or ``None``. See `load()` for
+        more information.
+      trigger_events:
+        If ``True``, trigger ``'before-save'`` and ``'after-save'`` events
+        for each setting. If ``False``, these events are not triggered.
     
     Returns:
-    
       A `PersistorResult` instance describing the result, particularly in the
       case of a failure. See `PersistorResult` for more information.
     """
@@ -238,14 +259,21 @@ class Persistor:
     return statuses_per_source, messages_per_source
   
   @classmethod
-  def clear(cls, setting_sources=None):
+  def clear(
+        cls,
+        setting_sources: Union[
+          Dict[str, Union[sources_.Source, List[sources_.Source]]],
+          List[str],
+          None,
+        ] = None
+  ):
     """Removes all settings from all specified setting sources.
     
-    Parameters:
-    
-    * `setting_sources` - Dictionary or list of setting sources or `None`. See
-      `load()` for more information. If there are no sources to clear, this
-      method has no effect.
+    Args:
+      setting_sources:
+        Dictionary or list of setting sources or ``None``. See `load()` for
+        more information. If there are no sources to clear, this method has
+        no effect.
     """
     if setting_sources is None:
       setting_sources = cls._DEFAULT_SETTING_SOURCES
@@ -305,7 +333,7 @@ class Persistor:
   
   @classmethod
   def _process_setting_sources(cls, setting_sources):
-    processed_setting_sources = collections.OrderedDict()
+    processed_setting_sources = {}
     
     if not isinstance(setting_sources, dict):
       for key in setting_sources:
@@ -340,7 +368,8 @@ class Persistor:
   def _list_settings(settings_or_groups, **walk_kwargs):
     settings = []
     for setting_or_group in settings_or_groups:
-      if isinstance(setting_or_group, Iterable):
+      # HACK: We do not use `isinstance(setting_or_group, Group)` to avoid a circular import.
+      if hasattr(setting_or_group, 'walk'):
         group = setting_or_group
         settings.append(group)
         settings.extend(list(group.walk(**walk_kwargs)))
@@ -358,59 +387,62 @@ PersistorResult = collections.namedtuple(
 The names of parameters below are parameters passed to `Persistor.load()` or
 `Persistor.save()`.
 
-Attributes:
+``settings_not_loaded`` contains all settings not loaded and ``statuses`` 
+contains statuses for each source individually.
 
-* `status` - Value indicating whether loading or saving proceeded with success
-  or some form of failure.
+Args:
+  status:
+    Value indicating whether loading or saving proceeded with success or some
+    form of failure. Possible values:
 
-  * `SUCCESS` - All settings were successfully loaded or saved.
+    * ``SUCCESS`` - All settings were successfully loaded or saved.
+    
+    * ``PARTIAL_SUCCESS`` - This status is returned when at least one of the
+     following occurs:
+      
+      * Only some settings were successfully loaded.
+      
+      * At least one source was not found when attempting to read from it.
+      
+      * Reading from at least one source was successful and failed for at least
+        one other source.
+      
+      * Writing to at least one source was successful and failed for at least
+        one other source.
+    
   
-  * `PARTIAL_SUCCESS` - This status is returned when at least one of the
-    following occurs:
+    * ``FAIL`` - This status is returned when at least one of the following
+      occurs:
     
-    * Only some settings were successfully loaded.
-    
-    * At least one source was not found when attempting to read from it.
-    
-    * Reading from at least one source was successful and failed for at least
-      one other source.
-    
-    * Writing to at least one source was successful and failed for at least one
-      other source.
-    
-    The `settings_not_loaded` attribute contains all settings not loaded and the
-    `statuses` attribute contains statuses for each source individually.
+      * Reading from/writing to all sources failed.
+      
+      * The ``setting_sources`` parameter is ``None`` and the default sources
+        returned by `Persistor.get_default_setting_sources()` is an empty
+        dictionary.
+      
+      * The ``setting_sources`` parameter is a list of keys (source names) and
+        there is at least one key not present in the default sources as returned
+        by `Persistor.get_default_setting_sources()`.
   
-  * `FAIL` - This status is returned when at least one of the following occurs:
-  
-    * Reading from/writing to all sources failed.
+    * ``NO_SETTINGS`` - Used when the ``settings_or_groups`` parameter is empty.
+  settings_not_loaded
+    List of settings and groups that were not loaded when calling 
+    `Persistor.load()`. These settings were not found in any setting source 
+    or they were skipped if their ``setting_sources`` attribute is not 
+    ``None`` and does not contain the key identifying the group of sources (
+    see `set_default_setting_sources()` for more information on the key.). 
+    For `Persistor.save()`, this attribute is always empty.
+  statuses_per_source
+    ``status`` values for each setting source passed to `Persistor.load()` or 
+    `Persistor.save()`. It is a dictionary of (setting source, status) pairs. 
+    Beside values in the ``status`` attribute, the following values can also 
+    appear in ``statuses_per_source``:
     
-    * The `setting_sources` parameter is `None` and the default sources returned
-      by `Persistor.get_default_setting_sources()` is an empty dictionary.
-    
-    * The `setting_sources` parameter is a list of keys (source names) and there
-      is at least one key not present in the default sources as returned by
-      `Persistor.get_default_setting_sources()`.
-  
-  * `NO_SETTINGS` - Used when the `settings_or_groups` parameter is empty.
-
-* `settings_not_loaded` - List of settings and groups that were not loaded when
-  calling `Persistor.load()`. These settings were not found in any setting
-  source or they were skipped if their `setting_sources` attribute is not `None`
-  and does not contain the key identifying the group of sources (see
-  `set_default_setting_sources()` for more information on the key.).
-  For `Persistor.save()`, this attribute is always empty.
-
-* `statuses_per_source` - `status` values for each setting source passed to
-  `Persistor.load()` or `Persistor.save()`. It is a dictionary of
-  (setting source, status) pairs. Beside values in the `status` attribute, the
-  following values can also appear in `statuses_per_source`:
-  
-  * `SOURCE_NOT_FOUND` - The source was not found when attempting to load
-    settings, either because the corresponding file is missing or the source
-    name does not exist.
-
-* `messages_per_source` - Messages for each setting source passed to
-  `Persistor.load()` or `Persistor.save()`. It is a dictionary of
-  (setting source, message) pairs.
+    * ``SOURCE_NOT_FOUND`` - The source was not found when attempting to load
+      settings, either because the corresponding file is missing or the source
+      name does not exist.
+  messages_per_source
+    Messages for each setting source passed to `Persistor.load()` or 
+    `Persistor.save()`. It is a dictionary of (setting source, message) 
+    pairs.
 """
