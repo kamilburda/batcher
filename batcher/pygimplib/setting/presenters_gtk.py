df6d0c9c7d11b53025bd8f00e70e424b907789b2
kamilburda <8431034+kamilburda@users.noreply.github.com>
2023-11-17 16:26:51 +0100

    presenters_gtk: Force minimum height for `ArraySetting` GUI

--- batcher/pygimplib/setting/presenters_gtk.py
+++ batcher/pygimplib/setting/presenters_gtk.py
@@ -744,7 +744,7 @@ class ArrayBoxPresenter(GtkPresenter):
     self._widget.disconnect(self._item_changed_event_handler_id)
     self._item_changed_event_handler_id = None
   
-  def _create_widget(self, setting, width=300, max_height=150, **kwargs):
+  def _create_widget(self, setting, width=300, min_height=200, max_height=300, **kwargs):
     def _add_existing_element(array_element_value, index):
       return self._add_array_element(setting[index], array_box)
     
@@ -767,6 +767,7 @@ class ArrayBoxPresenter(GtkPresenter):
     )
 
     array_box.set_property('width-request', width)
+    array_box.set_property('min-content-height', min_height)
     array_box.set_property('max-content-height', max_height)
     
     array_box.on_add_item = _add_existing_element
