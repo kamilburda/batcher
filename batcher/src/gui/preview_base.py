f4a62c45508662ebfa47e6b8c21070410d2932f6
kamilburda <8431034+kamilburda@users.noreply.github.com>
2023-11-24 18:58:23 +0100

    Update `src.gui.preview_base`

--- batcher/src/gui/preview_base.py
+++ batcher/src/gui/preview_base.py
@@ -1,5 +1,7 @@
 """Base class for preview widgets."""
 
+from typing import Callable, Optional
+
 import gi
 gi.require_version('Gtk', '3.0')
 from gi.repository import Gtk
@@ -15,7 +17,7 @@ class Preview(Gtk.Box):
     
     self._functions_to_invoke_at_update = []
   
-  def update(self):
+  def update(self) -> bool:
     """Updates the preview if update is not locked (see `lock_update()`)."""
     if self._update_locked:
       return True
@@ -26,21 +28,22 @@ class Preview(Gtk.Box):
     
     return False
   
-  def lock_update(self, lock, key=None):
-    """
-    If `lock` is `True`, calling `update` will have no effect. Passing `False`
-    to `lock` will enable updating the preview again.
+  def lock_update(self, lock: bool, key: Optional[str] = None):
+    """Prevents or allows updating the preview via `update()`.
+
+    If ``lock`` is ``True``, calling `update()` will have no effect. If ``lock``
+    is ``False``, the preview will be updated on subsequent calls to `update()`.
+
+    If ``key`` is specified to lock the update, the same key must be
+    specified to unlock the preview. Multiple keys can be used to lock the
+    preview; to unlock the preview, call this method with each of the keys.
     
-    If `key` is specified to lock the update, the same key must be specified to
-    unlock the preview. Multiple keys can be used to lock the preview; to unlock
-    the preview, call this method with each of the keys.
+    If ``key`` is specified and ``lock`` is ``False`` and the key was not
+    used to lock the preview before, nothing happens.
     
-    If `key` is specified and `lock` is `False` and the key was not used to lock
-    the preview before, nothing happens.
-    
-    If `key` is `None`, lock/unlock the preview regardless of which function
-    called this method. Passing `None` also removes previous keys that were used
-    to lock the preview.
+    If ``key`` is ``None``, the preview is locked/unlocked regardless of
+    which function called this method. Passing ``None`` also removes previous
+    keys that were used to lock the preview.
     """
     if key is None:
       self._lock_keys.clear()
@@ -54,9 +57,8 @@ class Preview(Gtk.Box):
       
       self._update_locked = bool(self._lock_keys)
   
-  def add_function_at_update(self, func, *func_args, **func_kwargs):
-    """
-    Add a function to a list of functions to invoke at the beginning of
+  def add_function_at_update(self, func: Callable, *func_args, **func_kwargs):
+    """Adds a function to a list of functions to invoke at the beginning of
     `update()`.
     
     The functions will be invoked in the order in which they were added and
