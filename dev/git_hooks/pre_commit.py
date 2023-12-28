817d607ef80bb1079493caf5d813d9e1a62ff251
kamilburda <8431034+kamilburda@users.noreply.github.com>
2023-12-28 20:49:06 +0100

    pre_commit: Run docs syncing only from the main branch

--- dev/git_hooks/pre_commit.py
+++ dev/git_hooks/pre_commit.py
@@ -18,6 +18,33 @@ sys.path.append(ROOT_DIRPATH)
 from dev import sync_docs
 
 
+def main():
+  repo = git.Repo(ROOT_DIRPATH)
+
+  if repo.active_branch.name == 'main':
+    run_sync_docs(repo)
+
+
+def run_sync_docs(repo):
+  staged_filepaths = [
+    os.path.normpath(os.path.join(ROOT_DIRPATH, diff.a_path))
+    for diff in repo.index.diff('HEAD')]
+
+  filepaths_to_sync = sync_docs.get_filepaths(sync_docs.PATHS_TO_PREPROCESS_FILEPATH)
+  filepaths_to_sync.extend(sync_docs.get_filepaths(sync_docs.PATHS_TO_COPY_FILEPATH))
+
+  sync_docs.main()
+
+  synced_filepaths_to_stage = (
+    get_synced_files_to_stage(staged_filepaths, filepaths_to_sync))
+
+  filtered_synced_filepaths_to_stage = [
+    filepath for filepath in synced_filepaths_to_stage
+    if not filepath_matches_gitignore(repo, filepath)]
+
+  repo.git.add(filtered_synced_filepaths_to_stage)
+
+
 def get_synced_files_to_stage(staged_filepaths, filepaths_to_sync):
   return [
     filepaths_to_sync[staged_filepath]
@@ -31,28 +58,6 @@ def filepath_matches_gitignore(repo, filepath):
     return False
   else:
     return True
-
-
-def main():
-  repo = git.Repo(ROOT_DIRPATH)
-  
-  staged_filepaths = [
-    os.path.normpath(os.path.join(ROOT_DIRPATH, diff.a_path))
-    for diff in repo.index.diff('HEAD')]
-  
-  filepaths_to_sync = sync_docs.get_filepaths(sync_docs.PATHS_TO_PREPROCESS_FILEPATH)
-  filepaths_to_sync.extend(sync_docs.get_filepaths(sync_docs.PATHS_TO_COPY_FILEPATH))
-  
-  sync_docs.main()
-  
-  synced_filepaths_to_stage = (
-    get_synced_files_to_stage(staged_filepaths, filepaths_to_sync))
-  
-  filtered_synced_filepaths_to_stage = [
-    filepath for filepath in synced_filepaths_to_stage
-    if not filepath_matches_gitignore(repo, filepath)]
-  
-  repo.git.add(filtered_synced_filepaths_to_stage)
 
 
 if __name__ == '__main__':
