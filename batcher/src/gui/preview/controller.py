6a0bf574b29e0e69415a442936fe95d7b05d69a2
kamilburda <8431034+kamilburda@users.noreply.github.com>
2024-01-29 21:35:56 +0100

    Improve fix for Scale procedure not work correctly in image preview

--- batcher/src/gui/preview/controller.py
+++ batcher/src/gui/preview/controller.py
@@ -210,7 +210,8 @@ class PreviewsController:
   
   def _connect_update_rendering_of_image_preview(self):
     def _after_add_action(actions, action, orig_action_dict, builtin_actions):
-      if action['orig_name'].value not in builtin_actions or action['orig_name'].value == 'scale':
+      if (action['origin'].is_item('gimp_pdb')
+          or (action['origin'].is_item('builtin') and action['orig_name'].value == 'scale')):
         self._custom_actions[action.get_path()] = action
         
         _update_rendering_of_image_preview(action['enabled'])
