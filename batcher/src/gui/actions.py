46069ce6ee3a6390d8d07da221bf17a03c1b6a4f
kamilburda <8431034+kamilburda@users.noreply.github.com>
2024-02-01 22:12:29 +0100

    gui.actions: Ellipsize description in action edit dialog

--- batcher/src/gui/actions.py
+++ batcher/src/gui/actions.py
@@ -470,8 +470,8 @@ class _ActionEditDialog(GimpUi.Dialog):
   _MORE_OPTIONS_SPACING = 4
   _MORE_OPTIONS_BORDER_WIDTH = 4
 
-  _LABEL_PROCEDURE_NAME_MAX_WIDTH_CHARS = 50
-  _LABEL_PROCEDURE_DESCRIPTION_MAX_WIDTH_CHARS = 50
+  _LABEL_ACTION_NAME_MAX_WIDTH_CHARS = 50
+  _LABEL_ACTION_DESCRIPTION_MAX_WIDTH_CHARS = 50
   
   def __init__(self, action, pdb_procedure, *args, **kwargs):
     super().__init__(*args, **kwargs)
@@ -492,7 +492,7 @@ class _ActionEditDialog(GimpUi.Dialog):
     self._label_procedure_name.label.set_ellipsize(Pango.EllipsizeMode.END)
     self._label_procedure_name.label.set_markup(
       '<b>{}</b>'.format(GLib.markup_escape_text(action['display_name'].value)))
-    self._label_procedure_name.label.set_max_width_chars(self._LABEL_PROCEDURE_NAME_MAX_WIDTH_CHARS)
+    self._label_procedure_name.label.set_max_width_chars(self._LABEL_ACTION_NAME_MAX_WIDTH_CHARS)
     self._label_procedure_name.connect('changed', self._on_label_procedure_name_changed, action)
 
     self._label_procedure_description = None
@@ -550,8 +550,9 @@ class _ActionEditDialog(GimpUi.Dialog):
       label=summary,
       xalign=0.0,
       yalign=0.5,
+      ellipsize=Pango.EllipsizeMode.END,
       wrap=True,
-      max_width_chars=self._LABEL_PROCEDURE_DESCRIPTION_MAX_WIDTH_CHARS,
+      max_width_chars=self._LABEL_ACTION_DESCRIPTION_MAX_WIDTH_CHARS,
     )
     if full_description:
       label_description.set_tooltip_text(full_description)
