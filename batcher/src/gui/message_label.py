da5e8f09282c50e54e750dbbea964818259e7744
kamilburda <8431034+kamilburda@users.noreply.github.com>
2024-02-02 22:11:42 +0100

    message_label: Replace "More" popup with tooltip

--- batcher/src/gui/message_label.py
+++ batcher/src/gui/message_label.py
@@ -3,8 +3,6 @@
 from typing import Optional
 
 import gi
-gi.require_version('Gdk', '3.0')
-from gi.repository import Gdk
 from gi.repository import GLib
 from gi.repository import GObject
 gi.require_version('Gtk', '3.0')
@@ -13,53 +11,27 @@ from gi.repository import Pango
 
 import pygimplib as pg
 
-from src.gui import popup_hide_context as popup_hide_context_
-
 
 class MessageLabel(Gtk.Box):
   """A widget to display a label, and optionally additional information in a
-  popup below the label.
+  tooltip.
 
-  The popup is also available if the label text does not fit the width of the
-  parent widget.
+  The tooltip is also displayed if the label text does not fit the width of
+  the parent widget.
   """
-  
-  _MESSAGE_AND_MORE_BUTTON_SPACING = 2
-  _MORE_BUTTON_LABEL_AND_ARROW_SPACING = 5
-  
-  _TEXT_VIEW_MARGIN = 3
-  
-  _POPUP_WIDTH = 400
-  _MAX_POPUP_HEIGHT = 200
+  _SPACING = 2
   
   def __init__(self):
     super().__init__(homogeneous=False)
     
     self._label_text = ''
-    self._popup_text_lines = []
+    self._tooltip_text_lines = []
     self._message_type = None
     self._clear_delay = None
     
     self._init_gui()
     
-    self._popup_hide_context = popup_hide_context_.PopupHideContext(
-      self._popup_more,
-      self._button_more,
-      widgets_to_exclude_from_triggering_hiding=[
-        self._popup_more,
-        self._scrolled_window_more.get_hscrollbar(),
-        self._scrolled_window_more.get_vscrollbar(),
-      ],
-    )
-    self._popup_hide_context.enable()
-    
     self._label_message.connect('size-allocate', self._on_label_message_size_allocate)
-
-    self._button_more.connect('clicked', self._on_button_more_clicked)
-    self._button_more.connect('realize', self._on_button_more_realize)
-    
-    self._popup_more.connect('show', self._on_popup_more_show)
-    self._popup_more.connect('hide', self._on_popup_more_hide)
   
   def set_text(
         self,
@@ -70,18 +42,17 @@ class MessageLabel(Gtk.Box):
     """Sets the text of the label. The text is displayed in bold style.
     
     If the text is too wide to fit the label or the text has multiple lines,
-    the label is ellipsized and a button is displayed that displays a popup
-    containing the full text when clicked. Only the first line is displayed
-    in the label.
+    the label is ellipsized and a tooltip is displayed containing the full text.
+
+    Only the first line is displayed in the label.
     
     If ``clear_delay`` is not ``None`` and ``message_type`` is not
     `Gtk.MessageType.ERROR`, the message automatically disappears after the
-    specified delay in milliseconds. The timer is stopped if the popup is
-    displayed and restarted if the popup gets hidden.
+    specified delay in milliseconds.
     """
     if not text:
       self._label_text = ''
-      self._popup_text_lines = []
+      self._tooltip_text_lines = []
       self._label_message.set_text(self._label_text)
       return
     
@@ -93,7 +64,7 @@ class MessageLabel(Gtk.Box):
       first_line += '.'
     
     self._label_text = first_line
-    self._popup_text_lines = lines[1:]
+    self._tooltip_text_lines = lines[1:]
     self._message_type = message_type
     self._clear_delay = clear_delay
     
@@ -110,96 +81,18 @@ class MessageLabel(Gtk.Box):
       yalign=0.5,
       ellipsize=Pango.EllipsizeMode.END,
     )
-    
-    self._label_button_more = Gtk.Label(
-      label=_('_More'),
-      use_underline=True,
-    )
-    
-    self._hbox_button_more = Gtk.Box(
-      orientation=Gtk.Orientation.HORIZONTAL,
-      spacing=self._MORE_BUTTON_LABEL_AND_ARROW_SPACING,
-    )
-    self._hbox_button_more.pack_start(self._label_button_more, True, True, 0)
 
-    arrow = Gtk.Arrow(
-      arrow_type=Gtk.ArrowType.DOWN,
-      shadow_type=Gtk.ShadowType.IN,
-    )
-    self._hbox_button_more.pack_start(arrow, False, False, 0)
-    
-    self._button_more = Gtk.Button(relief=Gtk.ReliefStyle.NONE)
-    self._button_more.add(self._hbox_button_more)
-    self._button_more.show_all()
-    self._button_more.hide()
-    self._button_more.set_no_show_all(True)
-    
-    self._text_view_more = Gtk.TextView(
-      wrap_mode=Gtk.WrapMode.WORD,
-      left_margin=self._TEXT_VIEW_MARGIN,
-      right_margin=self._TEXT_VIEW_MARGIN,
-      editable=False,
-    )
-    
-    self._scrolled_window_more = Gtk.ScrolledWindow(
-      hscrollbar_policy=Gtk.PolicyType.AUTOMATIC,
-      vscrollbar_policy=Gtk.PolicyType.NEVER,
-      shadow_type=Gtk.ShadowType.ETCHED_IN,
-    )
-    self._scrolled_window_more.add(self._text_view_more)
-    
-    self._popup_more = Gtk.Window(
-      type=Gtk.WindowType.POPUP,
-      resizable=False,
-      type_hint=Gdk.WindowTypeHint.TOOLTIP,
-      width_request=self._POPUP_WIDTH,
-    )
-    self._popup_more.set_attached_to(self._button_more)
-    self._popup_more.add(self._scrolled_window_more)
-
-    self._scrolled_window_more.show_all()
-    
-    self.set_spacing(self._MESSAGE_AND_MORE_BUTTON_SPACING)
-
+    self.set_spacing(self._SPACING)
     self.pack_start(self._label_message, True, True, 0)
-    self.pack_start(self._button_more, False, False, 0)
   
   def _on_label_message_size_allocate(self, label, allocation):
     if ((pg.gui.get_label_full_text_width(self._label_message) > self.get_allocation().width)
-        or len(self._popup_text_lines) >= 1):
-      self._button_more.show()
+        or len(self._tooltip_text_lines) >= 1):
+      lines = list(self._tooltip_text_lines) + [self._label_text]
+
+      self._label_message.set_tooltip_text('\n'.join(lines).strip())
     else:
-      self._button_more.hide()
-  
-  def _on_button_more_clicked(self, button):
-    lines = list(self._popup_text_lines)
-    
-    if (pg.gui.get_label_full_text_width(self._label_message)
-        > self._label_message.get_allocation().width):
-      lines.insert(0, self._label_text)
-    
-    text = '\n'.join(lines).strip()
-
-    self._text_view_more.set_buffer(Gtk.TextBuffer(text=text))
-
-    self._popup_more.show()
-
-    absolute_label_position = pg.gui.get_position_below_widget(self)
-    if absolute_label_position is not None:
-      self._popup_more.move(*absolute_label_position)
-
-  def _on_button_more_realize(self, button):
-    self._popup_more.set_transient_for(pg.gui.utils.get_toplevel_window(self._button_more))
-
-  def _on_popup_more_show(self, popup):
-    self._popup_more.set_screen(self._button_more.get_screen())
-    
-    if self._message_type != Gtk.MessageType.ERROR:
-      self._timeout_remove(self._clear_delay, self.set_text)
-  
-  def _on_popup_more_hide(self, popup):
-    if self._message_type != Gtk.MessageType.ERROR:
-      self._timeout_add_strict(self._clear_delay, self.set_text, None)
+      self._label_message.set_tooltip_text(None)
   
   def _timeout_add_strict(self, delay, func, *args, **kwargs):
     if self._should_clear_text_after_delay(delay):
