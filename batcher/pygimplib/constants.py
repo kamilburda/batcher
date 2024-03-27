a49b6ea1685cc91b5a265e55a09c51f7774192a2
kamilburda <8431034+kamilburda@users.noreply.github.com>
2024-03-27 22:05:16 +0100

    constants: Remove `__all__`

--- batcher/pygimplib/constants.py
+++ batcher/pygimplib/constants.py
@@ -1,12 +1,5 @@
 """Constants used in other modules."""
 
-__all__ = [
-  'GIMP_ITEM_PATH_SEPARATOR',
-  'MENU_PATH_SEPARATOR',
-  'TEXT_FILE_ENCODING',
-]
-
-
 GIMP_ITEM_PATH_SEPARATOR = '/'
 MENU_PATH_SEPARATOR = '/'
 TEXT_FILE_ENCODING = 'utf-8'
